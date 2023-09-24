38e4a6d931b1e3f0b006c137da2ccf87f14bfab1
Ernest <ernestwinata@gmail.com>
2023-09-24 08:54:56 -0700

    initial commit

--- server/app.py
+++ server/app.py
@@ -16,28 +16,59 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username'],
-            password_hash=json['password']
-        )
-        db.session.add(user)
-        db.session.commit()
-        return user.to_dict(), 201
+        
+        username = request.get_json()['username']
+        password = request.get_json()['password']
 
-class CheckSession(Resource):
-    pass
+        if username and password:
+            
+            new_user = User(username=username)
+            new_user.password_hash = password
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['user_id'] = new_user.id
+            
+            return new_user.to_dict(), 201
+
+        return {'error': '422 Unprocessable Entity'}, 422
 
 class Login(Resource):
-    pass
+
+    def post(self):
+        
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user.authenticate(password):
+
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {"message": "Invalid username or password."}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = db.session.get(User, user_id)
+            if user:
+                return user.to_dict(), 200
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
